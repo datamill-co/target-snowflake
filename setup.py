56ea42f05f2c9e4a566b4a882bd9ad2568458be9
Andrew Madonna <andrew.w.madonna@gmail.com>
2019-10-24 10:44:14 -0400

    Update version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='target-snowflake',
     url='https://github.com/datamill-co/target-snowflake',
     author='datamill',
-    version="0.0.1",
+    version="0.0.2",
     description='Singer.io target for loading data into Snowflake',
     long_description=long_description,
     long_description_content_type='text/markdown',
